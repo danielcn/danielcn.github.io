b28683fd8988312bad5661764b8276a909f9eb1f
Daniel Nascimento <danascimen@thoughtworks.com>
2020-05-15 00:25:55 -0300

    Addin routes for all pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './components/about/About'
 import Home from './components/home/Home'
+import Portifolio from './components/portiflio/Portiflio'
+import Service from './components/service/Service'
 
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/service">Services</Link>
+            </li>
+            <li>
+              <Link to="/portifolio">Portiflio</Link>
+            </li>
+            <li>
+              <Link to="/">Curriculum</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/service">
+            <Service />
+          </Route>
+          <Route path="/portifolio">
+            <Portifolio />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
