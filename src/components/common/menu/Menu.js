e499e1de457d5eb3ddeea45891d59fdc6a197999
Daniel Nascimento <danascimen@thoughtworks.com>
2020-05-15 00:39:16 -0300

    Adding menu component

---
+++ src/components/common/menu/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Menu = () => {
+  return(
+    <nav>
+      <h1 class="logo">AjaniTech</h1>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Portifolio</a></li>
+        <li><a href="https://www.linkedin.com/in/daniel-nascimento/" target="_blank">Curriculum</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Menu;
