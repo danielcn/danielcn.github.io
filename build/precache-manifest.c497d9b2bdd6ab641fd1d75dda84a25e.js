8f16ebb53589f4d9f55555cf0ab231b5409d91fa
Daniel Nascimento <danascimen@thoughtworks.com>
2020-05-21 00:20:15 -0300

    Add build production folder

---
+++ build/precache-manifest.c497d9b2bdd6ab641fd1d75dda84a25e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdbaa67d8b0f49fa4d877f0d2878b940",
+    "url": "/danielcn.github.io/index.html"
+  },
+  {
+    "revision": "cff67cfa7b8a7397bc50",
+    "url": "/danielcn.github.io/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "febf9defa0abc1c6041d",
+    "url": "/danielcn.github.io/static/js/2.98e46a10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/danielcn.github.io/static/js/2.98e46a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cff67cfa7b8a7397bc50",
+    "url": "/danielcn.github.io/static/js/main.cfc07e58.chunk.js"
+  },
+  {
+    "revision": "c92c595198ddc18a771c",
+    "url": "/danielcn.github.io/static/js/runtime-main.eda7982c.js"
+  }
+]);
